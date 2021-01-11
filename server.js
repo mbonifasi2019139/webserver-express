16de261ebb673385e4c88ba796e0679edba48283
mbonifasi2019139 <mbonifasi-2019139@kinal.edu.gt>
2021-01-10 19:43:02 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const hbs = require("hbs");
+require("./hbs/helpers");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+// Express HBS Engine
+hbs.registerPartials(__dirname + "/views/partials/");
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        nombre: "Marcos",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/data", (req, res) => {
+    res.send("Hola data");
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+});
